f00c0f5f2d39cd392a6670d1ef17317694f19f1c
Illia Krytskyi <I.Krytskiy@evnedev.com>
2023-01-09 21:58:57 +0200

    add router

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import './styles/css/responsive.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'font-awesome/css/font-awesome.min.css';
 import App from './components/App/App';
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
